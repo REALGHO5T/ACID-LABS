6afd21f491bfff562b6f52b7fdaeb89b2dac1254
Ad3an <144132964+Ad3an@users.noreply.github.com>
2024-11-05 22:57:00 +0530

    Updated

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-function lenis(){
-    // Initialize a new Lenis instance for smooth scrolling
-const lenis = new Lenis();
+function lenis() {
+  // Initialize a new Lenis instance for smooth scrolling
+  const lenis = new Lenis();
 
-// Listen for the 'scroll' event and log the event data to the console
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
+  // Listen for the 'scroll' event and log the event data to the console
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
 
-lenis.on('scroll', ScrollTrigger.update);
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000); // Convert time from seconds to milliseconds
-});
-gsap.ticker.lagSmoothing(0);
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+  gsap.ticker.lagSmoothing(0);
 }
-function pageview(){
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+function pageview() {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
-});
+  });
 }
 
-pageview()
-lenis()
+pageview();
+// lenis();
 
